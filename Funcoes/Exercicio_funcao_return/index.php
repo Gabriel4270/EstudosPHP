7ff9fba79f83c7f6adc18a758d9fdc7dfa6d2df8
Gabriel Silveira <gabrielsilveira12349@gmail.com>
2025-07-26 06:45:12 -0300

    adicao de return na funcao do numero ao quadrado

---
+++ Funcoes/Exercicio_funcao_return/index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ *Crie uma funcao que receba numero;
+ *Retorne o valor deste numero ao quadrado;
+*/
+
+    function retorno($numero){
+        return $numero**2;
+
+    }
+
+    echo retorno(7);
+?>
