1dc66fe6f2013602840b38361b0d220c861306ec
Gabriel Silveira <gabrielsilveira12349@gmail.com>
2025-07-26 05:45:33 -0300

    adicionei uma funcao com variaveis e as multipliquei e chamei a funcao ao final

---
+++ Funcoes/Exercicio_funcao2/exercicio2.php
@@ -0,0 +1,21 @@
+<?php
+/**                                             Atividade                                               
+ * 
+ * 
+ * 
+ * Crie uma funcao
+ * Defina tres variaveis numericas dentro dela
+ * Exiba a multiplicacao destes numeros com um echo
+ */
+    function multi(){
+        $numero1 = 7;
+        $numero2 = 5;
+
+        $total = $numero1*$numero2;
+
+        echo $numero1."x".$numero2." = ".$total;
+
+    }
+    multi();
+
+?>
