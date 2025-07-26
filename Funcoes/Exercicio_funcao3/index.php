23cf2ba077886f05d48358201374dd054df84222
Gabriel Silveira <gabrielsilveira12349@gmail.com>
2025-07-26 05:53:42 -0300

    exercicio fazer uma funcao, dentro de funcao fazer variaveis nome e sobrenome onde apareca ambos e ao final chame a funcao

---
+++ Funcoes/Exercicio_funcao3/index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Crie uma funcao;
+ * Define uma variavel nome e sobrenome;
+ * Imprima os valores concatenados;
+ */
+
+    function nomeSobrenome(){
+        $nome = "Gabriel";
+        $sobrenome = "Silveira";
+
+        echo $nome." ".$sobrenome;
+
+    }
+    nomeSobrenome();
+?>
