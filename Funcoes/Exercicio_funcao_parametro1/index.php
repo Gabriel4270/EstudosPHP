518f54f52e7d103f6bb1f96ebfc927f2b0a857ef
Gabriel Silveira <gabrielsilveira12349@gmail.com>
2025-07-26 06:05:25 -0300

    exercicio sobre parametros na funcao adicionando apenas nome e idade e a chamando no final do codigo

---
+++ Funcoes/Exercicio_funcao_parametro1/index.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Crie uma funcao;
+     * Ela deve receber um parametro de nome e idade;
+     * imprima "Ola eu sou NOME  e tenho X anos"
+     */
+    
+    function pessoa($nome, $idade){
+        echo "Ola eu sou ".$nome."  e tenho ".$idade." anos";
+    }
+          pessoa("Gabriel", 21);
+?>
