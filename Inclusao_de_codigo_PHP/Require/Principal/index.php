d20fb176c933ad5b6ea75d04ae7b22ad787e2600
Gabriel Silveira <gabrielsilveira12349@gmail.com>
2025-07-26 03:49:57 -0300

    diferenciando o include de require e utilizando o DIR dentro do PHP

---
+++ Inclusao_de_codigo_PHP/Require/Principal/index.php
@@ -0,0 +1,6 @@
+<?php
+ //require serve para basicamente a mesma coisa que o include, porem caso de erro ele para diferente do inlcude que mesmo com erro ele nao interrompe o programa
+    require __DIR__ ."/../teste.php";
+ //DIR serve para nao errar o caminho para pastas anteriores como nao tinha mais pastas o teste ficou avulso apos os ".."
+echo $number; ?>
+
