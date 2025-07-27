26d68be3986d5fe5cee96cce8abb88e40d175641
Gabriel Silveira <gabrielsilveira12349@gmail.com>
2025-07-27 03:24:04 -0300

    Exercicio modificando array com list as transformando em variaveis

---
+++ Manipulação_de_arrays/Exercicio_array_list/index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Crie um array com os seguintes valores: jaguar, 3.0, azul, 18, Teto solar, automatico;
+ * Chame este array de carro;
+ * Crie variaveis com base neste array;
+*/
+
+$carro = ["jaguar", "3.0", "azul", 18, "Teto solar", "automatico"];
+list($marca, $motor, $cor, $aro, $detalhe, $cambio)=$carro;
+
+echo $marca."<br>";
+echo $motor."<br>";
+echo $cor."<br>";
+echo $aro."<br>";
+echo $detalhe."<br>";
+echo $cambio."<br>";
+
+
+?>
