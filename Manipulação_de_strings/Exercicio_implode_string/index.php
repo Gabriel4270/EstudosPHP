fa71a4e9d413ad19eecfd45dc23b6bdc6783569c
Gabriel Silveira <gabrielsilveira12349@gmail.com>
2025-07-27 01:33:15 -0300

    Exercicio com implode fazendo uma array virar apenas uma string

---
+++ Manipulação_de_strings/Exercicio_implode_string/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Converta a seguinte array para uma string
+ * ["O", "PHP","é","muito","legal"]
+*/
+
+
+$array = ["O", "PHP","é","muito","legal"];
+$convercao = implode(" ",$array);
+
+echo $convercao;
+
+?>
